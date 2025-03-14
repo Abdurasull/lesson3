36386a724f07f7450a9e6608eef41dbbda66a72c
Abdurasull <abdurasulsalimov94@gmail.com>
2025-03-14 16:50:11 +0500

    web qismi tayyor

--- web_appllication/app.py
+++ web_appllication/app.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import json
 from flask import Flask, render_template, request, url_for
 from pprint import pprint
@@ -21,11 +22,20 @@ def user():
     users = []
     with open("db/users.json", "r", encoding="utf-8") as f:
         users = json.loads(f.read())
-        print((request.form))
-    if request.form['username'] == 'admin':
-        return render_template("regestr.html", error="This username is not available, please try another")
-    else:
-        return render_template("user.html", username=request.form['username'])
+        # print((filter(lambda user: user['email'] == request.form['email'], users )))
+        x = list(filter(lambda user: user['email'] == request.form['email'], users ))
+        print(len(x) == 0)
+        pprint(type(len(list(x))))
+        print(request.form['email'])
+        if len(x) == 0:
+            users.append(request.form)
+            pprint(users)
+            with open("db/users.json", "w", encoding="utf-8") as f:
+                f.write(json.dumps(users, indent=4, ensure_ascii=False))
+            return render_template("index.html")    
+            # print(users)
+        else:
+            return render_template("regestr.html", error="Bu email da foydalanuvchi ruyxatdan o`tgan, iltimos bosh email orqali urunib ko`ring")
 
 if __name__ == "__main__":
     app.run(debug=True)
