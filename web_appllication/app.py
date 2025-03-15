c33418f0248d77558ea679f3debc2c4744d59f56
Abdurasull <abdurasulsalimov94@gmail.com>
2025-03-15 14:19:33 +0500

    finish

--- web_appllication/app.py
+++ web_appllication/app.py
@@ -5,7 +5,7 @@ from pprint import pprint
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", endpoint="home")
 def home():
     return render_template("index.html")
 
@@ -22,20 +22,40 @@ def user():
     users = []
     with open("db/users.json", "r", encoding="utf-8") as f:
         users = json.loads(f.read())
-        # print((filter(lambda user: user['email'] == request.form['email'], users )))
+        
         x = list(filter(lambda user: user['email'] == request.form['email'], users ))
-        print(len(x) == 0)
-        pprint(type(len(list(x))))
-        print(request.form['email'])
-        if len(x) == 0:
+        if request.form['password'] == request.form['repate_password']:            
+            if len(x) == 0:
+                users.append(request.form)
+            
+                with open("db/users.json", "w", encoding="utf-8") as f:
+                    f.write(json.dumps(users, indent=4, ensure_ascii=False))
+                return render_template("user.html", user=request.form)    
+                # print(users)
+            else:
+                return render_template("regestr.html", error="Bu email da foydalanuvchi ruyxatdan o`tgan, iltimos boshqa email orqali urunib ko`ring")
+        else:
+            return render_template("regestr.html", error="Parollar mos emas")
+
+
+@app.route("/user_log", methods=["POST"])
+def user_log():
+    users = []
+    with open("db/users.json", "r", encoding="utf-8") as f:
+        users = json.loads(f.read())
+        
+        x_email = list(filter(lambda user: user['email'] == request.form['email'], users ))
+
+        if len(x_email) and (x_email[0]['password']  == request.form['password']):
             users.append(request.form)
-            pprint(users)
+        
             with open("db/users.json", "w", encoding="utf-8") as f:
                 f.write(json.dumps(users, indent=4, ensure_ascii=False))
-            return render_template("index.html")    
+            return render_template("user.html", user=request.form)    
             # print(users)
         else:
-            return render_template("regestr.html", error="Bu email da foydalanuvchi ruyxatdan o`tgan, iltimos bosh email orqali urunib ko`ring")
+            return render_template("login.html", error="Email yoki parol xato")
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
